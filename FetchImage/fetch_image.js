6f07540e5cc662ee18ee73460a7eac7abce8351f
blenk01 <morel.t@outlook.com>
2022-01-15 16:36:29 +0100

    Added small nodejs script to download images of preprocessed data

---
+++ FetchImage/fetch_image.js
@@ -0,0 +1,48 @@
+const dataForge = require('data-forge-fs');
+const fs = require('fs');
+const client = require('https');
+const util = require('util');
+
+var BASE_URL = "https://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg"
+var WIDTH = 512
+var HEIGHT = 512
+var BASE_FOLDER = "../Datas/Images/"
+
+async function downloadImage(objId, ra, dec) {
+    return new Promise((resolve) => {
+        url = util.format("%s?ra=%s&dec=%s&width=%s&height=%s", BASE_URL, ra, dec, WIDTH, HEIGHT)
+        client.get(url, (res) => {
+            let file = fs.createWriteStream(BASE_FOLDER + objId + ".jpeg")
+            res.pipe(file)
+            file.on("close", () => { resolve() })
+        }).on("error", (e) => console.log(e))
+    }, (error) => { console.log(error)})
+}
+
+async function downloadImages(df, step) {
+    var nbProcessed = 0
+    var size = df.toRows().length
+    while ( nbProcessed < size ) {
+        let dfToProcess = df.skip(nbProcessed).take(step)
+        let tabPromise = []
+        dfToProcess.forEach( row => {
+            let objId = row["OBJID"]
+            let ra = row["RA"]
+            let dec = row["DEC"]
+            tabPromise.push(downloadImage(objId, ra, dec))
+        })
+        await Promise.all(tabPromise)
+        nbProcessed += step
+        console.log(util.format("%d/%d", nbProcessed, size))
+    }
+}
+
+
+var toSkip = 0
+var step = 300
+var df = dataForge.readFileSync("../Datas/dataWithPreProcess.csv")
+    .parseCSV()
+    .subset(["OBJID", "RA", "DEC"])
+    .skip(toSkip)
+
+downloadImages(df, step)
